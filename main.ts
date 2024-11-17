17f93ab910e4c7d789f88a594a9cb66b85c809b4
Mike Kasprzak <mike@distraction.engineer>
2024-11-17 16:06:30 -0500

    Added stub for XML parsing

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 import { Application, Router } from "@oak/oak";   // Router: handles URL requests
 import { Eta } from "@eta-dev/eta";               // Eta: HTML template engine (i.e. <%= data %>, <% javascript code %>) 
 import { DB } from "@sqlite";                     // DB: SQLite database access
+//import * as XML from "@xml";                      // XML parsing
 
 const router = new Router();
 const eta = new Eta({ views: import.meta.dirname });
 const db = new DB("test.db");
 const port = 8080;
+
+// Testing XML parsing
+//using file = await Deno.open("hello.xml");
+//console.log(XML.parse(file));
 
 
 // Handle GET requests for the root URL
